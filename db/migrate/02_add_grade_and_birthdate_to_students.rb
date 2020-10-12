3541beaf54f88c5155054210f34a888ca1d41cd8
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-12 21:39:08 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,1 +1,9 @@
-class AddGradeAndBirthdayToStudents < ActiveRecord::Migration[5.2]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
+  
+  def change
+    add_column :students do |t|
+      t.integer :grade 
+      t.string :birtdate 
+    end 
+    
+end
